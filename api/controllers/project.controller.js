12a0a6a2cb89c79dae37e29bb496318096c67b2c
mo-adel007 <67648966+mo-adel007@users.noreply.github.com>
2024-07-10 14:53:01 +0300

    Add files via upload

---
+++ api/controllers/project.controller.js
@@ -0,0 +1,128 @@
+import Project from "../models/project.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const createProject = async (req, res, next) => {
+  try {
+    const project = await Project.create(req.body);
+    return res.status(201).json(project);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateProject = async (req, res, next) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    if (!project) {
+      return next(errorHandler(404, 'Project not found!'));
+    }
+
+    const updatedProject = await Project.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedProject);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteProject = async (req, res, next) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    if (!project) {
+      return next(errorHandler(404, 'Project not found!'));
+    }
+
+    await Project.findByIdAndDelete(req.params.id);
+    res.status(200).json({ success: true, message: 'Project has been deleted!' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProject = async (req, res, next) => {
+  try {
+    const project = await Project.findById(req.params.id).populate('community', 'name');
+    if (!project) {
+      return next(errorHandler(404, 'Project not found!'));
+    }
+    res.status(200).json(project);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProjects = async (req, res, next) => {
+  try {
+    const projects = await Project.find().populate('community', 'name');
+    res.status(200).json(projects);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getFeaturedProject = async (req, res, next) => {
+  try {
+    const featuredProjects = await Project.find({ featured: true }).populate('community', 'name');
+    res.status(200).json(featuredProjects);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getProjectByType = async (req, res, next) => {
+  try {
+    const { type } = req.params;
+    const projects = await Project.find({ type, featured: true }).populate('community', 'name');
+    console.log(`Fetched ${type} Projects:`, projects); // Debugging line
+    res.status(200).json(projects);
+  } catch (error) {
+    console.error('Error fetching listings by type:', error); // Debugging line
+    next(error);
+  }
+};
+
+export const getProjectByCommunity = async (req, res, next) => {
+  try {
+    const { community } = req.params;
+    console.log(`Fetching projects for community: ${community}`); // Debugging line
+    const projects = await Project.find({ community }).populate('community', 'name');
+    console.log(`Fetched Projects:`, projects); // Debugging line
+    res.status(200).json(projects);
+  } catch (error) {
+    console.error('Error fetching projects by community:', error); // Debugging line
+    next(error);
+  }
+};
+
+export const getProjectByDeveloper = async (req, res, next) => {
+  try {
+    const { developer } = req.params;
+    const projects = await Project.find({ developer }).populate('community', 'name');
+    console.log(`Fetched Projects:`, projects); // Debugging line
+    res.status(200).json(projects);
+  } catch (error) {
+    console.error('Error fetching listings by type:', error); // Debugging line
+    next(error);
+  }
+};
+
+export const getProjectsByQuery = async (req, res, next) => {
+  try {
+    const { type, category, bedrooms, location, deliveryDate } = req.query;
+    let filter = {};
+
+    if (type) filter.type = type;
+    if (category) filter.typeOfUnit = category;
+    if (bedrooms && type !== "New Projects") filter.bedrooms = bedrooms;
+    if (deliveryDate && type === "New Projects") filter.deliveryDate = deliveryDate;
+    if (location) filter.address = { $regex: location, $options: "i" };
+
+    const projects = await Project.find(filter).populate('community', 'name');
+    res.status(200).json(projects);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ api/routes/project.route.js
@@ -0,0 +1,31 @@
+// src/routes/project.route.js
+import express from "express";
+import { verifyToken } from "../utils/verifyUser.js";
+import {
+  createProject,
+  updateProject,
+  deleteProject,
+  getProject,
+  getProjects,
+  getFeaturedProject,
+  getProjectByType,
+  getProjectByDeveloper,
+  getProjectByCommunity,
+  getProjectsByQuery
+} from "../controllers/project.controller.js";
+
+const router = express.Router();
+
+router.post("/create", verifyToken, createProject);
+router.post("/update/:id", verifyToken, updateProject);
+router.delete("/delete/:id", verifyToken, deleteProject);
+router.get("/get/:id", getProject);
+router.get("/projects", getProjects);
+router.get("/featured-projects", getFeaturedProject);
+router.get("/projects/:type", getProjectByType);
+router.get("/projects/developer/:developer", getProjectByDeveloper);
+router.get("/projects/community/:community", getProjectByCommunity);
+router.get("/search", getProjectsByQuery);
+
+
+export default router;
